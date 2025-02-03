f34044c1d6dc28675e20adcbf58b05a646900ad6
Odin-Johan <nidojohan@gmail.com>
2025-02-03 15:07:41 +0100

    lagos er kul

--- src/app/(pages)/hjem/page.tsx
+++ src/app/(pages)/hjem/page.tsx
@@ -1,10 +1,17 @@
+import Navbar from "@/components/navbar/navbar";
 import React from "react";
 
 export default function Home() {
   return (  
-    <main className="w-screen">
+    <main className="w-screen overflow-x-hidden">
         <title>DFS-Nettside | Hjem</title>
-        <h1 className="h-[30vh] text-[3vw] p-10 md:font-bold ">DET FRIVILLIGE SKYTTERVESEN</h1>
+        <div className=" mt-24 leading-[90%] text-4xl sm:text-8xl p-10 md:font-bold flex flex-col ">
+          <h1>DET</h1> 
+          <h1>FRIVILLIGE</h1>
+          <h1>SKYTTERVESEN</h1>
+          <p className="sm:text-xl text-sm">- Forsvar, sport og god våpenkultur</p> 
+          </div>
+          <section className="w-screen h-screen"></section>
     </main>
   );
 };
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,11 +1,14 @@
 "use client";
-
+import Image from "next/image";
+import logo from "@/public/logo.svg"
+import hamburger from "@/public/hamburger.svg"
 export default function Navbar() {
     return (
-        <>        
-            <div className="closed"></div>
-
-            <div className="open"></div>
+        <>
+            <div className=" fixed top-0 w-full h-24 flex items-center justify-between p-5  bg-[--accent]">
+                <Image className="h-16 sm:h-22" alt="Logo til norgescuppen" src={logo} />
+                <Image className="h-16  sm:h-22 sm:hidden block" alt="Logo SQL" src={hamburger} />
+            </div>
         </>
     );
 };
