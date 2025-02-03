879f4a22e6e116dc35a9d164a357231adb1ff65c
Odin-Johan <nidojohan@gmail.com>
2025-02-03 14:09:36 +0100

    endring til /hjem

--- src/app/(pages)/hjem/page.tsx
+++ src/app/(pages)/hjem/page.tsx
@@ -1,9 +1,9 @@
 
 export default function Home() {
-  return (
+  return (  
     <main className="w-screen">
         <title>DFS-Nettside | Hjem</title>
-        <p></p>
+        <h1 className="h-[30vh] text-[3vw] p-10 md:font-bold ">DET FRIVILLIGE SKYTTERVESEN</h1>
     </main>
   );
 };
