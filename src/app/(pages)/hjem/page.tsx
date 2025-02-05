6a270185cf41e7f5ddf1ae9fe2b269d01b4f1edb
Sidney <afs0027201@agderskolen.no>
2025-02-05 12:54:21 +0100

    dfslogo og link til hjemmesiden

--- src/app/(pages)/hjem/page.tsx
+++ src/app/(pages)/hjem/page.tsx
@@ -3,18 +3,29 @@
 import React from "react";
 import InfiniteScroll from "@/components/infinite-scroll";
 import SisteNyttSlider from "@/components/siste-nytt-slider";
+import Overnatting from "@/components/overnatting";
+import Image from "next/image";
+import dfslogo from "@/public/dfslogo.png";
 
 export default function Home() {
   
+  const dfsnettside = () => {
+    window.location.href = "https://dfs.no/info","_blank";
+  };
+
   return (  
     <>
       <title>DFS-Nettside | Hjem</title>
       <header className="w-screen h-screen flex justify-center items-center">
-        <div className="container flex flex-col leading-[90%] text-4xl sm:text-8xl md:font-bold ">
+        <div className="container flex flex-row leading-[90%] text-4xl sm:text-8xl md:font-bold justify-between items-center ">
+          <div className="">
           <h1>DET</h1> 
           <h1>FRIVILLIGE</h1>
           <h1>SKYTTERVESEN</h1>
-          <p className="sm:text-xl text-sm">- Forsvar, sport og god våpenkultur</p> 
+          <p className="sm:text-xl text-sm">- Forsvar, sport og god våpenkultur</p>
+          </div>
+        <Image onClick={dfsnettside} className="w-[350px]" src={dfslogo} alt="dfslogo"></Image>
+      
         </div>
       </header>
       
