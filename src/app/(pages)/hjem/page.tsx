f74f79f8fdb5ac7297b965b7760e027bc3a05ebb
lhagfoss <afs0028447@agderskolen.no>
2025-02-07 13:00:28 +0100

    endret tittel tekst

--- src/app/(pages)/hjem/page.tsx
+++ src/app/(pages)/hjem/page.tsx
@@ -18,9 +18,9 @@ export default function Home() {
       <header className="w-screen h-screen flex justify-center items-center">
         <div className="container flex flex-row justify-between items-center">
           <div className="text-indigo-900 flex flex-col leading-[90%] text-4xl sm:text-8xl md:font-bold">
-            <h1 className="home-element">DET</h1> 
-            <h1 className="home-element">FRIVILLIGE</h1>
-            <h1 className="home-element">SKYTTERVESEN</h1>
+            <h1 className="home-element">NORGES</h1> 
+            <h1 className="home-element">CUPPEN</h1>
+            <h1 className="home-element">2025</h1>
           </div>
         <Image onClick={dfsnettside} className="w-[350px] hover:scale-105 active:scale-90 duration-300" src={dfslogo} alt="dfslogo"></Image>
       
