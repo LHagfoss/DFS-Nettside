4fdc5e34cf869abd8a9f4ae79a0a0b0f9ddb535d
lhagfoss <afs0028447@agderskolen.no>
2025-02-07 13:03:30 +0100

    endret tittel tekst

--- src/app/(pages)/hjem/page.tsx
+++ src/app/(pages)/hjem/page.tsx
@@ -9,7 +9,7 @@ import AnimateIn from "@/components/animate-in";
 
 export default function Home() {
   const dfsnettside = () => {
-    window.open("https://dfs.no/info", "_blank");
+    window.open("https://skyttertidende.no/norgescup-1681983756/sections/29717/articles", "_blank");
   };
 
   return (  
@@ -17,7 +17,7 @@ export default function Home() {
       <title>DFS-Nettside | Hjem</title>
       <header className="w-screen h-screen flex justify-center items-center">
         <div className="container flex flex-row justify-between items-center">
-          <div className="text-indigo-900 flex flex-col leading-[90%] text-4xl sm:text-8xl md:font-bold">
+          <div className="text-[#002f67] flex flex-col leading-[90%] text-4xl sm:text-8xl md:font-bold">
             <h1 className="home-element">NORGES</h1> 
             <h1 className="home-element">CUPPEN</h1>
             <h1 className="home-element">2025</h1>
