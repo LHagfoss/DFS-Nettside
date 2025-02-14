d2c52bd7fd1a2d6600c3e6fe7ece5198e8f32380
07Siddi <146091174+07Siddi@users.noreply.github.com>
2025-02-14 15:07:57 +0100

    Update page.tsx

--- src/app/(pages)/aktiviteter/page.tsx
+++ src/app/(pages)/aktiviteter/page.tsx
@@ -3,7 +3,7 @@ import Aktiviteter from "@/components/aktiviteter";
 export default function Home(){
     return(
         <div>
-        <title>DFS-Nettside | Aktiviteter</title>
+        <title>NC 2025 - Runde 1 | Aktiviteter</title>
         <Aktiviteter />
         </div>
     )
