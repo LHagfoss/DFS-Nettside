be869d85c5fd09a209680e253e652e9e809a7b30
07Siddi <146091174+07Siddi@users.noreply.github.com>
2025-02-14 15:10:26 +0100

    Update page.tsx

--- src/app/(pages)/stevneinfo/page.tsx
+++ src/app/(pages)/stevneinfo/page.tsx
@@ -3,7 +3,7 @@ import Stevneinfo from "@/components/stevneinfo";
 export default function Home(){
     return(
         <div>
-        <title>DFS-Nettside | Stevneinfo</title>
+        <title>NC 2025 - Runde 1 | Stevneinfo</title>
         <Stevneinfo />
         </div>
     )
